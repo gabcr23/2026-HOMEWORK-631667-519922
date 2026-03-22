8da83dab66e81e981204cc99474bdd15df0d97ca
danieleene <91674916+danieleene@users.noreply.github.com>
2026-03-22 15:29:59 +0100

    Aggiunta la classe Giocatore
    I CFU sono stati spostati sul Giocatore (con i relativi getter & setter)
    E' stato aggiunto un collegamento con Borsa

---
+++ src/it/uniroma3/diadia/Giocatore.java
@@ -0,0 +1,33 @@
+package it.uniroma3.diadia.giocatore;
+
+public class Giocatore {
+
+	
+	static final private int CFU_INIZIALI = 20;
+	
+	
+	private int cfu;
+	private Borsa borsa;
+	
+	
+	//Getter & Setter
+	
+	public int getCfu() {
+		return this.cfu;
+	}
+
+	public void setCfu(int cfu) {
+		this.cfu = cfu;		
+	}
+	
+	
+	public Borsa getBorsa() {
+		return this.borsa;
+	}
+	
+	//Costruttore
+	public Giocatore() {
+		this.cfu = CFU_INIZIALI;
+		this.borsa = new Borsa();
+	}
+}
