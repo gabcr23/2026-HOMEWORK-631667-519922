8745646253c2ceb8ef6275db5a1a26befab96808
Gabriele <101520381+gabcr23@users.noreply.github.com>
2026-03-22 14:48:34 +0100

    Add files via upload

---
+++ src/it/uniroma3/diadia/Partita.java
@@ -0,0 +1,79 @@
+package it.uniroma3.diadia;
+
+
+
+
+/**
+ * Questa classe modella una partita del gioco
+ *
+ * @author  docente di POO
+ * @see Stanza
+ * @version base
+ */
+
+public class Partita {
+
+	static final private int CFU_INIZIALI = 20;
+
+	private Labirinto labirinto;
+	
+	private Stanza stanzaCorrente;
+	private boolean finita;
+	private int cfu;
+	
+	public Partita(){
+		this.labirinto=new Labirinto();
+		this.stanzaCorrente=this.labirinto.getStanzaIniziale();
+		
+		this.finita = false;
+		this.cfu = CFU_INIZIALI;
+	}
+
+    /*
+     * Metodo creaStanze(), getter e setter delle stanze spostate in Labirinto
+    */
+	
+	public Stanza getStanzaVincente() {
+		return this.labirinto.getStanzaVincente();
+	}
+	
+	public void setStanzaCorrente(Stanza stanzaCorrente) {
+		this.stanzaCorrente=stanzaCorrente;
+	}
+	public Stanza getStanzaCorrente() {
+		return this.stanzaCorrente;
+	}
+	
+	
+	/**
+	 * Restituisce vero se e solo se la partita e' stata vinta
+	 * @return vero se partita vinta
+	 */
+	public boolean vinta() {
+		return this.getStanzaCorrente()== this.getStanzaVincente();
+	}
+
+	/**
+	 * Restituisce vero se e solo se la partita e' finita
+	 * @return vero se partita finita
+	 */
+	public boolean isFinita() {
+		return finita || vinta() || (cfu == 0);
+	}
+
+	/**
+	 * Imposta la partita come finita
+	 *
+	 */
+	public void setFinita() {
+		this.finita = true;
+	}
+
+	public int getCfu() {
+		return this.cfu;
+	}
+
+	public void setCfu(int cfu) {
+		this.cfu = cfu;		
+	}	
+}
